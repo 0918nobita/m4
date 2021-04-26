6e631f7e5425941977274a67790cf1e3e39fab35
0918nobita <nobita.0918@gmail.com>
2021-04-26 14:27:25 +0900

    :white_check_mark: テスト用のシェルスクリプトを追加した

---
+++ test.bash
@@ -0,0 +1,8 @@
+#!/bin/bash -eu
+find ./test -mindepth 1 -maxdepth 1 -type f -name "*.m4" | while read file
+do
+  name=$(basename $file .m4)
+  echo "[${name}]"
+  m4 -I./lib $file |& cmp test/__snapshots__/$name.snap
+  echo -e "  \033[1;32mSuccess\033[0m"
+done
